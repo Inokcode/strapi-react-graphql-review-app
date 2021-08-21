3cd323d775ad4ec903eaab743a62677e60ca47fd
Inokcode <regilearn@gmail.com>
2021-08-21 19:58:30 +0530

    fetch all data added

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import useFetch from '../hooks/useFetch';
+import { Link } from 'react-router-dom';
 
 export default function Homepage() {
+  const { loading, error, data } = useFetch('http://localhost:1337/reviews');
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
   return (
     <div>
-      homepage
+      {data.map((review) => (
+        <div key={review.id} className="review-card">
+          <div className="rating">{review.rating}</div>
+          <h2>{review.title}</h2>
+          <small>123</small>
+          <p>{review.body.substring(0, 200)}...</p>
+          <Link to={`/details/${review.id}`}>Read more</Link>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
